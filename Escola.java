118826adf0b5229d80e8219f65de2a0c48bd183d
andre <andrew.maike10@gmail.com>
2025-06-17 23:22:20 -0300

    note: improvements in the school class

--- Escola.java
+++ Escola.java
@@ -1,38 +1,58 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Escola {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         final double MEDIA_MINIMA = 7.0;
+        final int LIMITE_DE_NOTAS = 3;
 
-        System.out.println("-> Cadasto do Aluno <-");
-        Aluno aluno1 = new Aluno("Deivid Andrade");
-        aluno1.addNota(8.5);
-        aluno1.addNota(9.0);
-        aluno1.addNota(6.5);
+        String cadastrarNovoAluno = "s";
 
+        while (cadastrarNovoAluno.equalsIgnoreCase("s")){
+            System.out.print("\nInforme o nome do aluno(a): ");
+            String nomeAluno = scanner.nextLine();
 
-        double mediaAluno1 = aluno1.calcMedia();
+            Aluno aluno = new Aluno(nomeAluno);
 
-        System.out.printf("A media final do Aluno %s e: %.2f\n", aluno1.getNome(), mediaAluno1);
-        if(aluno1.verficarAprovacao(MEDIA_MINIMA)){
-            System.out.println("Situacao: APROVADO!\n");
-        }else {
-            System.out.println("Situacao: REPROVADO!\n");
+            System.out.println("<------------------------------------->");
+            System.out.println("Digite as notas de " + aluno.getNome() + ".");
+            System.out.println("Digite qualquer letra para finalizar");
+            System.out.println("<-------------------------------------->");
+
+            for(int i = 0; i < LIMITE_DE_NOTAS; i++){
+                System.out.print("Digite a " + (i + 1) + "a nota (ou uma letra para parar): ");
+
+                if (scanner.hasNextDouble()){
+                    double nota = scanner.nextDouble();
+                    aluno.addNota(nota);
+                } else {
+                    scanner.next();
+                    System.out.println("Entrada de notas finalizadas pelo usuario.");
+                    break;
+                }
+            }
+
+            System.out.println("\n--- Resultado para " + aluno.getNome() + "---");
+            double mediaFinal = aluno.calcMedia();
+            System.out.printf("Media final: %.1f\n", mediaFinal);
+
+            if(aluno.verficarAprovacao(MEDIA_MINIMA)){
+                System.out.println("Sistuacao: APROVADO!");
+            }else{
+                System.out.println("Situacao: REPROVADO!");
+            }
+            System.out.print("-----------------------------------------------\n");
+
+            System.out.print("Deseja cadastrar outro aluno? (s/n): ");
+            cadastrarNovoAluno = scanner.next();
+
+            scanner.nextLine();
         }
-
-        System.out.println("-> Cadasto do Aluno <-");
-        Aluno aluno2 = new Aluno("Deivid Andrade");
-        aluno1.addNota(8.5);
-        aluno1.addNota(9.0);
-        aluno1.addNota(6.5);
-
-        double mediaAluno2 = aluno2.calcMedia();
-
-        System.out.printf("A media final do Aluno %s e: %.2f\n", aluno2.getNome(), mediaAluno2);
-        if(aluno2.verficarAprovacao(MEDIA_MINIMA)){
-            System.out.println("Situacao: APROVADO!\n");
-        }else {
-            System.out.println("Situacao: REPROVADO!\n");
-        }
-
+        System.out.println("\nPrograma Finalizado.");
+        scanner.close();
     }
 }
